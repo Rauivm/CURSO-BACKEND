8346a896994b551f236be894e574d411a17d1d05
Raui <rauivm@hotmail.com>
2024-12-04 22:30:31 -0300

    Ignore .env

--- aula 01/arquivo.js
+++ aula 01/arquivo.js
@@ -2,11 +2,28 @@ const path = require('node:path')
 const fs = require('node:fs')
 
 const filePath = path.join(process.cwd(),'aula 01','texto.txt')
+const fileOutPath = path.join(process.cwd(), 'aula 01','texto-com-linhas.txt')
+console.log(filePath)
+
+console.time('leitura do arquivo')
+console.time('manipular arquivo')
 fs.readFile(filePath, {}, (erro,dados)=>{
     if(erro){
-        console.log(`Erro na leitura do arquivo no caminho ${filePath}`)
+        console.error(`Erro na leitura do arquivo no caminho ${filePath}`)
         return
     }
-    console.log(dados.toString())
+    console.timeEnd('leitura do arquivo')
+
+    const texto = dados.toString()
+    const linhas = texto.split('\n')
+
+    const linhasAjustadas = linhas.map((Linha, index, arrayDeLinhas) =>
+        `${index + 1} - ${Linha}`)
+    fs.writeFile(fileOutPath, linhasAjustadas.join('\n'), {}, (erro)=>{
+        if(erro){
+            console.error(`Erro na escrita do arquivo no caminho ${fileOutPath}`)
+            return
+        }
+    console.timeEnd('manipular arquivo')
+    })
 })
-console.log(filePath)
